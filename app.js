187c885ed6844b7000c0539988ccdf7f89cb0bba
James <jytojroob@gmail.com>
2021-08-18 20:07:58 -0700

    updated procfile

--- app.js
+++ app.js
@@ -85,13 +85,11 @@ if (!err) {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
